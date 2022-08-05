721616fa203a546c886b7307d25f4cc4cdfe245a
jobanpreet27 <mail@jobanpreet.ca>
2022-08-05 12:21:46 -0500

    add layout component

---
+++ components/Layout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "./Header";
+import Head from "next/head";
+import Script from "next/script";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <meta charSet='utf-8' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#000000' />
+        <meta
+          name='Winnipeg Transit WebApp'
+          content='Winnipeg Buses timetable'
+        />
+        <title>Winnipeg Transit Webapp </title>
+        <meta
+          name='Description'
+          content='Check the live bus timetable of the transit stops near your in Winnipeg .'
+        />
+        <meta
+          name='Keywords'
+          content='Winnipeg Transit, Winnipeg Bus timetable'
+        />
+      </Head>
+      <Header />
+      <div className='container'>{children}</div>
+      <Script src='https://cdn.onesignal.com/sdks/OneSignalSDK.js'></Script>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
 import useOneSignal from "../utils/useOneSignal";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   useOneSignal();
 
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
