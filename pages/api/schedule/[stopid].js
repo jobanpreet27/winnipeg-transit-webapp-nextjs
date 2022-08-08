4fc87bc48562eb02a2a352cfc559453af39d6896
jobanpreet27 <mail@jobanpreet.ca>
2022-08-07 21:17:38 -0500

    add schedule api

---
+++ pages/api/schedule/[stopid].js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  const { stopid } = req.query;
+  let schedule = [];
+  let url = new URL(
+    `https://api.winnipegtransit.com/v3/stops/${stopid}/schedule.json`
+  );
+  url.searchParams.append("api-key", process.env.WT_API_KEY);
+  url.searchParams.append("usage", "long");
+
+  const sortSchedule = (array) => {
+    array.forEach((routes) => {
+      routes["scheduled-stops"].forEach((stop) => {
+        schedule.push(stop);
+      });
+    });
+    schedule.sort((a, b) => {
+      return a.times.departure.estimated > b.times.departure.estimated ? 1 : -1;
+    });
+  };
+  //Fetch schedule
+  try {
+    let data = await fetch(url);
+    let stopSchedule = await data.json();
+    sortSchedule(stopSchedule["stop-schedule"]["route-schedules"]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error fetching schedule" });
+  }
+  res.status(200).send(schedule);
+}
