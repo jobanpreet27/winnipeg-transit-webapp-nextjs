e2fcc8b4ceabdbb46cc5561ba10204dbc97d85bd
jobanpreet27 <mail@jobanpreet.ca>
2022-08-09 12:11:44 -0500

    add create alert api

---
+++ pages/api/alert.js
@@ -0,0 +1,34 @@
+const axios = require("axios").default;
+
+export default function handler(req, res) {
+  if (req.method !== "POST") return;
+
+  const { userId, busId, stopId } = req.body;
+  const options = {
+    method: "POST",
+    url: "https://onesignal.com/api/v1/notifications",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Basic " + process.env.SIGNAL_REST_API_KEY,
+      "Content-Type": "application/json",
+    },
+    data: {
+      included_segments: ["include_player_ids"],
+      contents: { en: "Your bus is 5 mins away" },
+      include_player_ids: [userId],
+      name: "alert",
+      app_id: process.env.NEXT_PUBLIC_SIGNAL_APPID,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  res.status(201).send({ message: "Alert Created" });
+}
