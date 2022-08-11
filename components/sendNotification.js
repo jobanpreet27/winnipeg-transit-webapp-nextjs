893aa16756166ad772eed423d6e135fd5564715b
jobanpreet27 <mail@jobanpreet.ca>
2022-08-10 21:24:44 -0500

    add send notification function

---
+++ components/sendNotification.js
@@ -0,0 +1,29 @@
+const axios = require("axios").default;
+
+module.exports.sendNotification = function (userId, content) {
+  const options = {
+    method: "POST",
+    url: "https://onesignal.com/api/v1/notifications",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Basic " + process.env.SIGNAL_REST_API_KEY,
+      "Content-Type": "application/json",
+    },
+    data: {
+      included_segments: ["include_player_ids"],
+      contents: { en: content },
+      include_player_ids: [userId],
+      name: "alert",
+      app_id: process.env.NEXT_PUBLIC_SIGNAL_APPID,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
