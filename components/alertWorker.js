3dfd8f606dce4fcb84b79aad11dc4c3bd8269bf5
jobanpreet27 <mail@jobanpreet.ca>
2022-08-11 19:12:00 -0500

    add alert route and  worker

--- components/alertWorker.js
+++ components/alertWorker.js
@@ -1,12 +1,42 @@
-const { resolve } = require("path");
 const { workerData } = require("worker_threads");
-const { num, message } = workerData;
+const sendNotification = require("./sendNotification").sendNotification;
+const axios = require("axios");
+const moment = require("moment");
 
-const timer = () => new Promise((res) => setTimeout(res, 10000));
-async function load() {
-  for (let i = 0; i < num; i++) {
-    console.log(message, i);
+const { routeKey, stopId, busKey, userId } = workerData;
+const url = new URL(
+  `https://api.winnipegtransit.com/v3/stops/${stopId}/schedule.json`
+);
+url.searchParams.append("api-key", process.env.WT_API_KEY);
+let timeLeft = 100; // 100 minutes
+
+const timer = () => new Promise((res) => setTimeout(res, 30000));
+
+const getBusTime = async () => {
+  let time = 0;
+  const response = await axios(url.toString());
+  const schedule = response.data["stop-schedule"]["route-schedules"];
+  schedule.forEach((route) => {
+    if (route.route.key !== routeKey) return;
+    route["scheduled-stops"].every((bus) => {
+      if (bus.key === busKey) {
+        time = moment(bus.times.departure.estimated).diff(moment(), "minutes");
+        return false;
+      }
+      return true;
+    });
+  });
+
+  return time;
+};
+
+const load = async function () {
+  while (timeLeft > 5) {
+    console.log("loop ran");
     await timer();
+    timeLeft = await getBusTime();
+    console.log(timeLeft);
   }
-}
+  sendNotification(userId, `${routeKey}  will on the stop in ${timeLeft} mins`);
+};
 load();
--- pages/api/alert.js
+++ pages/api/alert.js
@@ -1,39 +1,11 @@
-const axios = require("axios").default;
-const { Worker } = require("worker_threads");
-const path = require("path");
+import { Worker } from "worker_threads";
+import sendNotification from "../../components/sendNotification";
+
 export default function handler(req, res) {
   if (req.method !== "POST") return;
-
-  const { userId, busId, stopId } = req.body;
-  const options = {
-    method: "POST",
-    url: "https://onesignal.com/api/v1/notifications",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Basic " + process.env.SIGNAL_REST_API_KEY,
-      "Content-Type": "application/json",
-    },
-    data: {
-      included_segments: ["include_player_ids"],
-      contents: { en: "Your bus is 5 mins away" },
-      include_player_ids: [userId],
-      name: "alert",
-      app_id: process.env.NEXT_PUBLIC_SIGNAL_APPID,
-    },
-  };
-
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-
+  const { userId, busKey, stopId, routeKey } = req.body;
   const worker = new Worker("./components/alertWorker.js", {
-    workerData: { num: 10, message: "Hello World" },
+    workerData: { userId, busKey, stopId, routeKey },
   });
-
   res.status(201).send({ message: "Alert Created" });
 }
