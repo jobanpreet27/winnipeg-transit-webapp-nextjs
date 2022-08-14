ff76225962cc340e01cff6483d1e1930acba2b67
jobanpreet27 <mail@jobanpreet.ca>
2022-08-14 13:26:15 -0500

    add routes info to stops

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardActionArea from "@mui/material/CardActionArea";
 import Container from "@mui/material/Container";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
 
 export default function Home() {
   const [userPosition, setUserPosition] = useState(null);
@@ -22,6 +24,22 @@ export default function Home() {
           <Card sx={{ width: "100%", marginBottom: 1 }} variant='outlined'>
             <CardActionArea>
               <CardHeader title={stop.name} subheader={stop.distances.direct} />
+              <CardContent sx={{ pt: 0, "&:last-child": { pb: 0 } }}>
+                {stop.routes.map((route) => (
+                  <Avatar
+                    sx={{
+                      display: "inline",
+                      mr: 1,
+                      px: 0.5,
+                      color: "#000000",
+                      bgcolor: route["badge-style"]["background-color"],
+                    }}
+                    variant='square'
+                  >
+                    {route.key}
+                  </Avatar>
+                ))}
+              </CardContent>
             </CardActionArea>
           </Card>
         </Link>
