6aa5e2a546e8741ed76fbaa28da1af54e32007ad
jobanpreet27 <mail@jobanpreet.ca>
2022-08-20 17:51:47 -0500

    add loader for loading

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import Stop from "../components/Stop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Home() {
-  const [userPosition, setUserPosition] = useState(null);
-  const [stops, setStops] = useState([]);
+  const [stops, setStops] = useState(null);
   const defaultCoords = {
     latitude: "49.852263",
     longitude: "-97.112128",
@@ -15,29 +15,40 @@ export default function Home() {
     let stopsList = await data.json();
     setStops(stopsList);
   };
+  const renderStops = () => {
+    if (!stops) {
+      return (
+        <CircularProgress
+          sx={{ position: "absolute", top: "45%", left: "45%" }}
+          size='4rem'
+        />
+      );
+    } else {
+      return stops.map((stop) => <Stop key={stop.key} stop={stop} />);
+    }
+  };
+
+  const getStops = async () => {
+    if (navigator.permissions) {
+      const result = await navigator.permissions.query({ name: "geolocation" });
+      if (result.state !== "granted") fetchStops(defaultCoords);
+    }
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition((position) =>
+        fetchStops(position.coords)
+      );
+    else {
+      fetchStops(defaultCoords);
+    }
+  };
 
   useEffect(() => {
-    const getStops = async () => {
-      if (navigator.permissions) {
-        const result = await navigator.permissions.query({ name: "geolocation" });
-        if (result.state !== "granted") fetchStops(defaultCoords);
-      }
-      if (navigator.geolocation)
-        navigator.geolocation.getCurrentPosition((position) =>
-          fetchStops(position.coords)
-        );
-      else {
-        fetchStops(defaultCoords);
-      }
-    };
     getStops();
   }, []);
 
   return (
-    <Container maxWidth='md' sx={{ p: 0 }}>
-      {stops.map((stop) => (
-        <Stop key={stop.key} stop={stop} />
-      ))}
+    <Container maxWidth='sm' sx={{ p: 0 }}>
+      {renderStops()}
     </Container>
   );
 }
